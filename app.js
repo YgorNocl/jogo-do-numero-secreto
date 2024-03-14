b978ecc08e594c88627b9b6d03ac303f9b214c55
Ygor <76566771+YgorNocl@users.noreply.github.com>
2024-03-14 05:33:04 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ let tentativas = 1;
 function exibirTextoNaTela(tag, texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
-    responsiveVoice.speak(texto, 'Brazilian Portuguese Female')
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.3})
 }
 function exibirMensagemInicial(){
     exibirTextoNaTela('h1', 'Jogo do Número Secreto');
-    exibirTextoNaTela('p', 'Escolha Um Número Entre 1 e 500');
+    exibirTextoNaTela('p', 'Escolha Um Número Entre 1 e 10');
 }
 
 exibirMensagemInicial();
@@ -17,7 +17,6 @@ function verificarChute() {
     
     let chute = document.querySelector('input').value;
     if (chute == numeroSecreto){
-        alert(`O Número: ${chute} está correto`);
         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
         exibirTextoNaTela('h1', `O Número: ${chute} está correto`);
         let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}`;
@@ -25,11 +24,9 @@ function verificarChute() {
         document.getElementById('reiniciar').removeAttribute('disabled');
 
     }else if(chute > numeroSecreto){
-        alert(`O Número secreto é menor que ${chute}`);
         exibirTextoNaTela('p', `O Número secreto é menor que ${chute}`);
 
     }else{
-        alert(`O Número secreto é maior que ${chute}`);
         exibirTextoNaTela('p', `O Número secreto é maior que ${chute}`);
     }
     tentativas ++;
